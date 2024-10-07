67c2cf897979addbfc52d47c1c54842478d2ee96
issamv3 <gbot2613@gmail.com>
2024-10-07 19:39:22 +0000

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get("/", async function (req, res) {
-  res.sendFile(path.join(__dirname, "my.html"));
+  res.sendFile(path.join(__dirname, "My.html"));
 });
 
 app.listen(port, () => {
