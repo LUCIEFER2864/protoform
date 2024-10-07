51b820ed112aa9bc2e2da7953ba74e35001638aa
issamv3 <gbot2613@gmail.com>
2024-10-07 19:37:36 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+const chalk = require("chalk");
+const app = express();
+app.use(express.json());
+
+const port = process.env.PORT || 3000;
+
+app.get("/", async function (req, res) {
+  res.sendFile(path.join(__dirname, "my.html"));
+});
+
+app.listen(port, () => {
+  console.log(chalk.hex('#ffcc00')(`رينننن بورت: ${port}`));
+});
+
+module.exports = app
